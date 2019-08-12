33bc14b08741bf0ad747724fac5750f06a8f0a3a
Hiroki Takano <midwhite.development@gmail.com>
2019-08-12 10:38:43 +0900

    introduce react-hooks as lint and useState / useEffect / useContext

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,20 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, useContext } from 'react';
 import './App.css';
 
+const LangContext = React.createContext('ja');
+
+const Child: React.FC = () => {
+  const value = useContext(LangContext);
+  return <h3>{value}</h3>;
+};
+
 const App: React.FC = () => {
+  const [{ num }, setClick] = useState({ num: 3 });
+  useEffect(() => {
+    const handler = () => {
+      document.title = `${window.innerWidth}`;
+    };
+    window.addEventListener('resize', handler);
+
+    return () => {
+      window.removeEventListener('resize', handler);
+    };
+  });
+
+  const [lang, setLang] = useState('ja');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <LangContext.Provider value={lang}>
+          <input type="text" placeholder="Keyword" />
+          <div>{num}</div>
+          <button onClick={() => setClick({ num: num + 1 })}>increment</button>
+          <Child />
+          <select name="lang" id="lang" onChange={e => setLang(e.target.value)}>
+            <option value="ja">JAPANESE</option>
+            <option value="en">ENGLISH</option>
+          </select>
+        </LangContext.Provider>
+      </div>
+    </>
   );
 };
 
