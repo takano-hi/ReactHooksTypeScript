ec86d1f101074fd0de5910b7e6633fed9458b4e1
Hiroki Takano <midwhite.development@gmail.com>
2019-08-14 22:49:35 +0900

    import React as default

--- react/src/containers/messages/MessageListContainer.tsx
+++ react/src/containers/messages/MessageListContainer.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 
 const MessageListContainer: React.FC = () => <h1>MessageListContainer</h1>;
 
--- react/src/containers/relations/RelationListContainer.tsx
+++ react/src/containers/relations/RelationListContainer.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 
 const RelationListContainer: React.FC = () => <h1>RelationListContainer</h1>;
 
