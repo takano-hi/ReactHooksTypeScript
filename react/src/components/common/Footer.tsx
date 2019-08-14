64fbc48abad44c697ca0c2a1a9ae1537fbaa4ef7
Hiroki Takano <midwhite.development@gmail.com>
2019-08-14 23:15:18 +0900

    add classnames

--- react/src/components/common/Footer.tsx
+++ react/src/components/common/Footer.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import classNames from 'classnames';
 
-const Footer: React.FC = () => <h2>Footer</h2>;
+import * as CSSModule from './Footer.module.scss';
+
+const Footer: React.FC = () => (
+  <footer id="footer">
+    <div className="row">
+      <div className="col-12">
+        <p className={classNames(CSSModule.copyright, 'p-2')}>2019 (C) All rights reserved by uni-bit</p>
+      </div>
+    </div>
+  </footer>
+);
 
 export default Footer;
